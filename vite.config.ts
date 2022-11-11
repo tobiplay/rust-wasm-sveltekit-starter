3da99ed80b4c7f5c3dcc21b6dcbc290244fdd98c
tobiplay <tobiplay@fa-meier.de>
2022-11-11 15:48:58 +0100

    Install Vite plugins for WASM

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
-export default config;
+export default defineConfig({
+	plugins: [sveltekit(), wasm(), topLevelAwait()],
+	server: {
+		fs: {
+			// You should be able to work with any files that are
+			// in the SvelteKit src directory themselves. Vite should
+			// be able to resolve them from the workspace root directly.
+			//
+			// If Vite struggles to resolve them, you may want to allow the
+			// server to access the entire workspace. Uncomment below and add the
+			// paths to the pkg that contains the compiled WASM files.
+			//
+			// allow: [
+			// 	// We have to search the workspace root because the Vite config
+			// 	// is in a subdirectory of the workspace root:
+			// 	searchForWorkspaceRoot(process.cwd()),
+			//
+			// 	// Our custom path to be allowed by the Vite server:
+			// 	'<path to pkg>'
+			// ]
+		}
+	}
+});
