2842806eb1d48eb739548b4cd45f84d0113408d7
tobiplay <tobiplay@fa-meier.de>
2022-11-11 15:58:22 +0100

    Add `edition` and test `wasm-pack build`

--- src/hello-wasm/src/lib.rs
+++ src/hello-wasm/src/lib.rs
@@ -1,22 +1,20 @@
 #![allow(unused_variables)]
-fn main() {
-    mod utils;
+mod utils;
 
-    use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::*;
 
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    #[cfg(feature = "wee_alloc")]
-    #[global_allocator]
-    static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-    #[wasm_bindgen]
-    extern "C" {
-        fn alert(s: &str);
-    }
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
 
-    #[wasm_bindgen]
-    pub fn greet() {
-        alert("Hello, wasm-game-of-life!");
-    }
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-game-of-life!");
 }
